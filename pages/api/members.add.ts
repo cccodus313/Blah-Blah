cc9dfbf79c097911d78afde126d6bd3f3f5257cc
cccodus313 <cccodus313@gmail.com>
2023-07-27 02:08:54 +0900

    Add:서버사이드 공통 에러 핸들링

--- pages/api/members.add.ts
+++ pages/api/members.add.ts
@@ -1,14 +1,16 @@
+import checkSupportMethod from '@/controllers/error/check_support_method';
 import MemberCtrl from '@/controllers/member.ctrl';
 import { NextApiRequest, NextApiResponse } from 'next';
+import handleError from '../../controllers/error/handle_error';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
   const supportMethod = ['POST'];
   try {
-    if (supportMethod.indexOf(method!) === -1) {
-    }
+    checkSupportMethod(supportMethod, method);
     await MemberCtrl.add(req, res);
   } catch (err) {
     console.error(err);
+    handleError(err, res);
   }
 }
